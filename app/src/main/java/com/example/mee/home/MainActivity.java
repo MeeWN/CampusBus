39cb2480996d4e504f372d1dfd6292c5c1a483b7
ewqdsacxz <patric_emmel@outlook.com>
2017-05-06 23:50:25 +0700

    delete Unuse import

--- app/src/main/java/com/example/mee/home/MainActivity.java
+++ app/src/main/java/com/example/mee/home/MainActivity.java
@@ -3,40 +3,22 @@ package com.example.mee.home;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-import android.media.Image;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.widget.TextView;
-
-
 import com.example.mee.home.core.CardAdapter;
 import com.example.mee.home.core.DBHelper;
 import com.example.mee.home.core.ReservationController;
-
 import org.json.JSONArray;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     int toggle = 0;
