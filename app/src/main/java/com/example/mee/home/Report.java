6b3aad32bb8a0547d65144717f6d427f21c35103
Nutt <warisa_nut@hotmail.com>
2017-05-18 20:43:56 +0700

    add BarChart

--- app/src/main/java/com/example/mee/home/Report.java
+++ app/src/main/java/com/example/mee/home/Report.java
@@ -7,13 +7,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
+
+import java.util.ArrayList;
+
 public class Report extends AppCompatActivity {
+
+    BarChart barChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        barChart = (BarChart) findViewById(R.id.bargraph);
+
+        ArrayList<BarEntry> barEntries = new ArrayList<>();
+        barEntries.add(new BarEntry(44f,0));
+        barEntries.add(new BarEntry(88f,1));
+        barEntries.add(new BarEntry(66f,2));
+        barEntries.add(new BarEntry(12f,3));
+        barEntries.add(new BarEntry(19f,4));
+        barEntries.add(new BarEntry(91f,5));
+        BarDataSet barDataSet = new BarDataSet(barEntries,"Dates");
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -23,7 +45,26 @@ public class Report extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
+        ArrayList<String> theDates = new ArrayList<>();
+        theDates.add("April");
+        theDates.add("May");
+        theDates.add("June");
+        theDates.add("July");
+        theDates.add("August");
+        theDates.add("September");
+
+        ArrayList<IBarDataSet> iBarDataSet  = new ArrayList<IBarDataSet>();
+
+        BarDataSet dataSets = new BarDataSet(barEntries,"Entrys");
+        iBarDataSet.add(dataSets);
+        BarData theData = new BarData(iBarDataSet);
+        barChart.setData(theData);
+
+        barChart.setTouchEnabled(true);
+        barChart.setDragEnabled(true);
+        barChart.setScaleEnabled(true);
     }
+
 
 }
